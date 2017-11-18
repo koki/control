245a409484ae41699ced33cba1af9f32a287d9f1
Kynan Rilee <kynan.rilee@gmail.com>
2017-11-18 13:30:08 -0800

    stop using cobra, so we can pass through all args to kubectl

--- cli/cmd/ctl/generic.go
+++ cli/cmd/ctl/generic.go
@@ -2,7 +2,6 @@ package ctl
 
 import (
 	"bytes"
-	"flag"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -10,17 +9,12 @@ import (
 	"os/exec"
 	"strings"
 
-	"github.com/spf13/cobra"
-
 	"github.com/koki/short/client"
 	"github.com/koki/short/parser"
-	shortutil "github.com/koki/short/util"
 )
 
-func KubectlGeneric(subCommand string, args []string) error {
+func KubectlGeneric(kubeArgs []string) error {
 	var err error
-	kubeArgs := []string{subCommand}
-	kubeArgs = append(kubeArgs, args...)
 	cmd := exec.Command("kubectl", kubeArgs...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
@@ -33,10 +27,8 @@ func KubectlGeneric(subCommand string, args []string) error {
 	return err
 }
 
-func KubectlGenericDashO(subCommand string, args []string, outputFormat string, kubeOutput io.Writer) error {
+func KubectlGenericDashO(kubeArgs []string, outputFormat string, kubeOutput io.Writer) error {
 	var err error
-	kubeArgs := []string{subCommand}
-	kubeArgs = append(kubeArgs, args...)
 	kubeArgs = append(kubeArgs, "-o", outputFormat)
 	cmd := exec.Command("kubectl", kubeArgs...)
 	cmd.Stdin = os.Stdin
@@ -50,13 +42,12 @@ func KubectlGenericDashO(subCommand string, args []string, outputFormat string, 
 	return err
 }
 
-func KubectlGenericWithFiles(subCommand string, args []string, kubeFiles io.Reader) error {
+func KubectlGenericWithFiles(kubeArgs []string, kubeFile string) error {
 	var err error
-	kubeArgs := []string{subCommand}
-	kubeArgs = append(kubeArgs, args...)
-	kubeArgs = append(kubeArgs, "-f", "-")
+
+	kubeArgs = append(kubeArgs, "-f", kubeFile)
 	cmd := exec.Command("kubectl", kubeArgs...)
-	cmd.Stdin = kubeFiles
+	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	err = cmd.Run()
@@ -67,13 +58,11 @@ func KubectlGenericWithFiles(subCommand string, args []string, kubeFiles io.Read
 	return err
 }
 
-func KubectlGenericWithFilesDashO(subCommand string, args []string, kubeFiles io.Reader, outputFormat string, kubeOutput io.Writer) error {
+func KubectlGenericWithFilesDashO(kubeArgs []string, kubeFile string, outputFormat string, kubeOutput io.Writer) error {
 	var err error
-	kubeArgs := []string{subCommand}
-	kubeArgs = append(kubeArgs, args...)
-	kubeArgs = append(kubeArgs, "-f", "-", "-o", outputFormat)
+	kubeArgs = append(kubeArgs, "-f", kubeFile, "-o", outputFormat)
 	cmd := exec.Command("kubectl", kubeArgs...)
-	cmd.Stdin = kubeFiles
+	cmd.Stdin = os.Stdin
 	cmd.Stdout = kubeOutput
 	cmd.Stderr = os.Stderr
 	err = cmd.Run()
@@ -84,107 +73,115 @@ func KubectlGenericWithFilesDashO(subCommand string, args []string, kubeFiles io
 	return err
 }
 
-func ShortGenericCmd(subCmd string, acceptsFiles bool) *cobra.Command {
-	var (
-		// filenames holds the input files that are to be converted to shorthand or kuberenetes native syntax
-		filenames []string
-		// output denotes the format of the converted data
-		outputFormat string
-	)
-
-	genericCmd := &cobra.Command{
-		Use:   subCmd,
-		Short: fmt.Sprintf("equivalent to 'kubectl %s'", subCmd),
-		RunE: func(cmd *cobra.Command, args []string) error {
-			var err error
-
-			useStdin := false
-			// TODO: support stdin again?
-			/*
-				if len(args) == 1 && args[0] == "-" {
-					glog.V(3).Info("using stdin for input data")
-					useStdin = true
-				}
-			*/
-
-			outputFormat = strings.ToLower(outputFormat)
-			if len(outputFormat) > 0 && outputFormat != "yaml" && outputFormat != "json" {
-				return shortutil.UsageErrorf("unexpected value %s for -o --output", outputFormat)
+func ProcessArgs(args []string) (filenames []string, outputFormat string, kubeArgs []string) {
+	filenames = []string{}
+	kubeArgs = []string{}
+	for i := 0; i < len(args); i++ {
+		arg := args[i]
+		switch arg {
+		case "-f":
+			if i < len(args)-1 {
+				i++
+				filenames = append(filenames, args[i])
+				continue
 			}
+		case "-o":
+			if i < len(args)-1 {
+				o := strings.ToLower(args[i+1])
 
-			kubeOutStream := &bytes.Buffer{}
-			if acceptsFiles {
-				var streams []io.ReadCloser
-				if useStdin {
-					streams = []io.ReadCloser{
-						os.Stdin,
-					}
-				} else {
-					streams, err = parser.OpenStreamsFromFiles(filenames)
-					if err != nil {
-						return err
-					}
-				}
-
-				kubeObjs, err := client.ConvertEitherStreamsToKube(streams)
-				if err != nil {
-					return nil
-				}
-				kubeStream := &bytes.Buffer{}
-				err = client.WriteObjsToYamlStream(kubeObjs, kubeStream)
-				if err != nil {
-					return err
-				}
-
-				if len(outputFormat) == 0 {
-					return KubectlGenericWithFiles(subCmd, args, kubeStream)
-				}
-
-				err = KubectlGenericWithFilesDashO(subCmd, args, kubeStream, outputFormat, kubeOutStream)
-				if err != nil {
-					return err
-				}
-			} else {
-				if len(outputFormat) == 0 {
-					return KubectlGeneric(subCmd, args)
-				}
-
-				err = KubectlGenericDashO(subCmd, args, outputFormat, kubeOutStream)
-				if err != nil {
-					return err
+				if o == "yaml" || o == "json" {
+					i++
+					outputFormat = o
+					continue
 				}
 			}
+		}
 
-			// Copy the contents of kubeOutStream in case parsing fails and we need to backtrack.
-			kokiObjs, err := client.ConvertKubeStreams([]io.ReadCloser{ioutil.NopCloser(bytes.NewBuffer(kubeOutStream.Bytes()))})
-			if err != nil {
-				// Couldn't parse output. Just pass it through.
-				_, _ = os.Stdout.Write(kubeOutStream.Bytes())
-				return nil
-			}
-
-			switch outputFormat {
-			case "yaml":
-				err = client.WriteObjsToYamlStream(kokiObjs, os.Stdout)
-			case "json":
-				err = client.WriteObjsToJSONStream(kokiObjs, os.Stdout)
-			}
-
-			return err
-		},
+		kubeArgs = append(kubeArgs, arg)
 	}
 
-	genericCmd.Flags().StringSliceVarP(&filenames, "filenames", "f", nil, "path or url to input files to read manifests")
-	genericCmd.Flags().StringVarP(&outputFormat, "output", "o", "", "output format (yaml|json)")
+	return
+}
 
-	// parse the go default flagset to get flags for glog and other packages in future
-	genericCmd.PersistentFlags().AddGoFlagSet(flag.CommandLine)
+func ShortGenericCmd(args []string) error {
+	var err error
+	filenames, outputFormat, kubeArgs := ProcessArgs(args)
 
-	// defaulting this to true so that logs are printed to console
-	_ = flag.Set("logtostderr", "true")
+	useStdin := false
+	if len(filenames) == 1 && filenames[0] == "-" {
+		useStdin = true
+	}
 
-	//suppress the incorrect prefix in glog output
-	_ = flag.CommandLine.Parse([]string{})
+	kubeOutStream := &bytes.Buffer{}
+	if len(filenames) > 0 {
+		var streams []io.ReadCloser
+		if useStdin {
+			streams = []io.ReadCloser{
+				os.Stdin,
+			}
+		} else {
+			streams, err = parser.OpenStreamsFromFiles(filenames)
+			if err != nil {
+				return err
+			}
+		}
 
-	return genericCmd
+		kubeObjs, err := client.ConvertEitherStreamsToKube(streams)
+		if err != nil {
+			return nil
+		}
+		kubeStream := &bytes.Buffer{}
+		err = client.WriteObjsToYamlStream(kubeObjs, kubeStream)
+		if err != nil {
+			return err
+		}
+
+		kubeFile, err := ioutil.TempFile("", "tmp-koki-manifest")
+		if err != nil {
+			return fmt.Errorf("couldn't create temporary file for manifest")
+		}
+		defer os.Remove(kubeFile.Name())
+		if _, err := kubeFile.Write(kubeStream.Bytes()); err != nil {
+			return fmt.Errorf("couldn't write manifest to temporary file")
+		}
+		if err := kubeFile.Close(); err != nil {
+			return fmt.Errorf("couldn't close temporary manifest file")
+		}
+
+		if len(outputFormat) == 0 {
+			return KubectlGenericWithFiles(kubeArgs, kubeFile.Name())
+		}
+
+		err = KubectlGenericWithFilesDashO(kubeArgs, kubeFile.Name(), outputFormat, kubeOutStream)
+		if err != nil {
+			return err
+		}
+	} else {
+		if len(outputFormat) == 0 {
+			return KubectlGeneric(kubeArgs)
+		}
+
+		err = KubectlGenericDashO(kubeArgs, outputFormat, kubeOutStream)
+		if err != nil {
+			return err
+		}
+	}
+
+	// Copy the contents of kubeOutStream in case parsing fails and we need to backtrack.
+	kokiObjs, err := client.ConvertKubeStreams([]io.ReadCloser{ioutil.NopCloser(bytes.NewBuffer(kubeOutStream.Bytes()))})
+	if err != nil {
+		// Couldn't parse output. Just pass it through.
+		_, _ = os.Stdout.Write(kubeOutStream.Bytes())
+		return nil
+	}
+
+	switch outputFormat {
+	case "yaml":
+		err = client.WriteObjsToYamlStream(kokiObjs, os.Stdout)
+	case "json":
+		err = client.WriteObjsToJSONStream(kokiObjs, os.Stdout)
+	}
+
+	return err
+
 }
--- cli/main.go
+++ cli/main.go
@@ -4,11 +4,11 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/koki/control/cli/cmd"
+	"github.com/koki/control/cli/cmd/ctl"
 )
 
 func main() {
-	if err := cmd.RootCmd.Execute(); err != nil {
+	if err := ctl.ShortGenericCmd(os.Args[1:]); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
